e52555dcd1a80521352499a598fed181d54a3c13
oguzhan <oguzhaneem@gmail.com>
2021-04-17 22:40:52 +0300

    View Model Factory created

---
+++ app/src/main/java/com/ogocer/coroutinesretrofitmvvm/ui/base/ViewModelFactory.kt
@@ -0,0 +1,20 @@
+package com.ogocer.coroutinesretrofitmvvm.ui.base
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.ogocer.coroutinesretrofitmvvm.data.api.ApiHelper
+import com.ogocer.coroutinesretrofitmvvm.data.repository.MainRepository
+import com.ogocer.coroutinesretrofitmvvm.ui.main.viewmodel.MainViewModel
+import java.lang.IllegalArgumentException
+
+class ViewModelFactory(private val apiHelper: ApiHelper) : ViewModelProvider.Factory{
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MainViewModel::class.java)){
+            return MainViewModel(MainRepository(apiHelper)) as T
+        }
+        throw IllegalArgumentException("Unknown class name!!!")
+    }
+
+
+}
