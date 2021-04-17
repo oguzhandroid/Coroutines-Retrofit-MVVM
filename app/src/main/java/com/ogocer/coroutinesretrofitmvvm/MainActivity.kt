3d8888262b610bdf43897fe41cfbfd316b2bfe9c
oguzhan <oguzhaneem@gmail.com>
2021-04-17 22:08:40 +0300

    initial commit

---
+++ app/src/main/java/com/ogocer/coroutinesretrofitmvvm/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ogocer.coroutinesretrofitmvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
